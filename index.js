ce964be86067aa7e297dd765b0c9ced275619eff
mayank0508 <thakurmayank0508@gmail.com>
2022-10-31 22:07:10 +0530

    node-read

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-const fs = require('fs'); // this is a code that we are using to use the fs module
+// const fs = require('fs'); // this is a code that we are using to use the fs module
 
-const hello = 'hello world';
-console.log(hello);
+// const hello = 'hello world';
+// console.log(hello);
+
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8') // this is the code we use to read a certain file, utf-8 will return the data in english
+console.log(textIn);
