128924b279557e918aa362669b518b844c095c49
mayank0508 <thakurmayank0508@gmail.com>
2022-10-31 22:17:57 +0530

    node-write

--- index.js
+++ index.js
@@ -7,3 +7,8 @@ const fs = require('fs');
 
 const textIn = fs.readFileSync('./txt/input.txt', 'utf-8') // this is the code we use to read a certain file, utf-8 will return the data in english
 console.log(textIn);
+
+
+const textOut = `This is what we know about avacadp: ${textIn}.\nCreated on ${Date.now()}`;
+fs.writeFileSync('./txt/output.txt', textOut);
+console.log('File Written');
